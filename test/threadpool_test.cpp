094f6b00ca82cd245c85989f4a1d3bbb8cbfef78
Rémi Fontan <remifontan@gmail.com>
2026-05-03 11:30:22 +1200

    concurrent threadpool test

--- test/threadpool_test.cpp
+++ test/threadpool_test.cpp
@@ -13,19 +13,20 @@ int main() {
   bvh::v2::ThreadPool threadpool;
   std::vector<std::thread> threads;
 
-  size_t num_threads = 20;
-  size_t num_tasks = 1;
+  size_t num_threads = 200;
+  size_t num_tasks = 10;
   size_t num_millis = 45;
 
   auto estimated_time = std::chrono::milliseconds(
       num_tasks * num_millis * num_threads / threadpool.get_thread_count());
 
-  std::cout << "estimated time:"
+  std::cout << "total estimated time:"
             << std::chrono::duration_cast<std::chrono::seconds>(estimated_time)
             << std::endl;
 
-  // monitoring thread
-  threadpool.push([&](size_t) {
+  // monitoring thread, simply print time so that is is obvious so when that we
+  // can
+  threads.emplace_back([&]() {
     auto delta = std::max(std::chrono::milliseconds(1), estimated_time / 10);
     auto elasped = std::chrono::milliseconds(0);
     auto start = std::chrono::system_clock::now();
@@ -33,34 +34,35 @@ int main() {
     while (std::chrono::system_clock::now() < start + estimated_time) {
       std::this_thread::sleep_for(delta);
       elasped += delta;
-      std::cout << "[time: " << elasped << " / " << estimated_time << "]"
-                << std::endl;
+      std::cout << "[estimated remaining time: " << estimated_time - elasped
+                << "]" << std::endl;
     }
+
+    std::cout << "[test should be done by now]" << std::endl;
   });
 
+  // submit lots of task concurrently
   for (size_t i = 0; i < num_threads; ++i) {
-
     threads.emplace_back([&]() {
+      bvh::v2::ThreadPool::TaskGroup tgroup;
       for (int j = 0; j < num_tasks; ++j) {
-        threadpool.push([&](size_t) {
+        threadpool.push(tgroup, [&](size_t) {
           std::this_thread::sleep_for(std::chrono::milliseconds(num_millis));
         });
       }
 
-      threadpool.wait();
+      threadpool.wait(tgroup);
     });
   }
 
   std::cout << "wait for " << threads.size() << " threads" << std::endl;
-
   for (auto &th : threads) {
     if (th.joinable()) {
       th.join();
     }
   }
 
-  threadpool.wait();
-
+  threadpool.wait_all();
   std::cout << "done" << std::endl;
   return 0;
 }
